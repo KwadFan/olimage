558f2c47310be48c760008614f6ee31e57f6d1c2
Stephan Wendel <me@stephanwe.de>
2023-03-26 13:15:10 +0200

    chore: wip
    Signed-off-by: Stephan Wendel <me@stephanwe.de>

---
+++ build-a20.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# qemu-user-static != 5.2 may have issues when building bullseye arm64 images
+# recommended host OS to build: debian buster amd64 with qemu-user-static from buster-backports
+apt-get install qemu-user-static
+
+
+# a20
+bash run.sh -v image A20-OLinuXino bullseye minimal    A20-OLinuXino-bullseye-minimal-$(date +%Y%m%d-%H%M%S).img
+bash run.sh -v image A20-OLinuXino bullseye base       A20-OLinuXino-bullseye-base-$(date +%Y%m%d-%H%M%S).img
+
+# release images
+
+# a20
+ARGS=-r bash run.sh -v image A20-OLinuXino bullseye minimal    A20-OLinuXino-bullseye-minimal-$(date +%Y%m%d-%H%M%S).img
+ARGS=-r bash run.sh -v image A20-OLinuXino bullseye base       A20-OLinuXino-bullseye-base-$(date +%Y%m%d-%H%M%S).img
