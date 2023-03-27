bab2bc1d80a071753c08d74630aa433d6277b1bd
Stephan Wendel <me@stephanwe.de>
2023-03-27 19:16:44 +0200

    chore: wip
    Signed-off-by: Stephan Wendel <me@stephanwe.de>

--- build-a20.sh
+++ build-a20.sh
@@ -6,4 +6,4 @@ apt-get install qemu-user-static
 
 
 # a20
-bash run_headless.sh -v image --size 500 --ssh --hostname ultimainsailos A20-OLinuXino bullseye minimal A20-OLinuXino-bullseye-minimal-$(date +%Y%m%d-%H%M%S).img
+bash run_headless.sh -v image --size 500 --ssh --hostname ultimainsailos A20-OLinuXino bullseye base A20-OLinuXino-bullseye-minimal-$(date +%Y%m%d-%H%M%S).img
