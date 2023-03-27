69545eb18cc22b07e2407741cad3b90b6aa77908
Stephan Wendel <me@stephanwe.de>
2023-03-27 19:19:03 +0200

    chore: wip
    Signed-off-by: Stephan Wendel <me@stephanwe.de>

--- olimage/filesystem/variants/base.py
+++ olimage/filesystem/variants/base.py
@@ -7,59 +7,67 @@ from olimage.filesystem.base import FileSystemBase
 
 
 class VariantBase(FileSystemBase):
-    stages = ['configure', 'cleanup']
-    variant = 'base'
+    stages = ["configure", "cleanup"]
+    variant = "base"
 
     @stamp
     @export
     @prepare
     def configure(self):
-
         # Copy resolv.conf
         with Console("Copying /etc/resolv.conf"):
-            Utils.shell.run('rm -vf {}/etc/resolv.conf'.format(self._build_dir), ignore_fail=True)
-            Utils.shell.run('cp -vf /etc/resolv.conf {}/etc/resolv.conf'.format(self._build_dir))
+            Utils.shell.run(
+                "rm -vf {}/etc/resolv.conf".format(self._build_dir), ignore_fail=True
+            )
+            Utils.shell.run(
+                "cp -vf /etc/resolv.conf {}/etc/resolv.conf".format(self._build_dir)
+            )
 
         # Install packages
         self._install_packages()
 
-        # Configure blueman
-        with Console("Configuring blueman"):
-            Setup.blueman()
+        # # Configure blueman
+        # with Console("Configuring blueman"):
+        #     Setup.blueman()
 
-        # Enabling auto-login
-        with Console("Enabling auto-login"):
-            Utils.install('/etc/lightdm/lightdm.conf')
+        # # Enabling auto-login
+        # with Console("Enabling auto-login"):
+        #     Utils.install('/etc/lightdm/lightdm.conf')
 
-        # Set default displaymanager
-        with Console("Setting default display-manager"):
-            Setup.displaymanager("lightdm")
+        # # Set default displaymanager
+        # with Console("Setting default display-manager"):
+        #     Setup.displaymanager("lightdm")
 
-        # post-install
-        with Console("Post-install tasks"):
-            Utils.shell.chroot("/bin/bash -c 'echo -en > /etc/modules-load.d/cups-filters.conf'", ignore_fail=True)
+        # # post-install
+        # with Console("Post-install tasks"):
+        #     Utils.shell.chroot("/bin/bash -c 'echo -en > /etc/modules-load.d/cups-filters.conf'", ignore_fail=True)
 
-            # meh broken light-locker in focal
-            Utils.shell.chroot('apt-get -y --purge remove light-locker', log_error=False)
+        #     # meh broken light-locker in focal
+        #     Utils.shell.chroot('apt-get -y --purge remove light-locker', log_error=False)
 
-            # xfce panel defaults
-            Utils.install('/etc/X11/Xsession.d/99olimex')
+        #     # xfce panel defaults
+        #     Utils.install('/etc/X11/Xsession.d/99olimex')
 
-            # set xfce background
-            Utils.shell.run('dpkg-divert --rename --add --divert /usr/share/backgrounds/xfce/xfce-stripes.png.real /usr/share/backgrounds/xfce/xfce-stripes.png')
-            Utils.shell.run('dpkg-divert --rename --add --divert /usr/share/backgrounds/xfce/xfce-blue.jpg.real /usr/share/backgrounds/xfce/xfce-blue.jpg')
-            Utils.install('/usr/share/backgrounds/xfce/xfce-stripes.png')
-            Utils.install('/usr/share/backgrounds/xfce/xfce-blue.jpg')
-            Utils.install('/usr/share/backgrounds/xfce/xfce-red.jpg')
+        #     # set xfce background
+        #     Utils.shell.run('dpkg-divert --rename --add --divert /usr/share/backgrounds/xfce/xfce-stripes.png.real /usr/share/backgrounds/xfce/xfce-stripes.png')
+        #     Utils.shell.run('dpkg-divert --rename --add --divert /usr/share/backgrounds/xfce/xfce-blue.jpg.real /usr/share/backgrounds/xfce/xfce-blue.jpg')
+        #     Utils.install('/usr/share/backgrounds/xfce/xfce-stripes.png')
+        #     Utils.install('/usr/share/backgrounds/xfce/xfce-blue.jpg')
+        #     Utils.install('/usr/share/backgrounds/xfce/xfce-red.jpg')
 
         # restore resolv.conf
         with Console("Restore /etc/resolv.conf"):
-            Utils.shell.run('rm -vf {}/etc/resolv.conf'.format(self._build_dir), ignore_fail=True)
-            Utils.shell.run('ln -nsf ../run/resolvconf/resolv.conf {}/etc/resolv.conf'.format(self._build_dir))
+            Utils.shell.run(
+                "rm -vf {}/etc/resolv.conf".format(self._build_dir), ignore_fail=True
+            )
+            Utils.shell.run(
+                "ln -nsf ../run/resolvconf/resolv.conf {}/etc/resolv.conf".format(
+                    self._build_dir
+                )
+            )
 
     @stamp
     @export(final=True)
     @prepare
     def cleanup(self):
         super().cleanup()
-
